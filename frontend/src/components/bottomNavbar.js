960f5dc7b0206546261bf965be6eee910a29013c
manzhu <11046073@ntub.edu.tw>
2024-09-18 20:16:39 +0800

    修改底部導覽列css

--- frontend/src/components/bottomNavbar.js
+++ frontend/src/components/bottomNavbar.js
@@ -2,8 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Col, Row } from 'react-bootstrap';
 import { HouseDoor, Grid, Cart, Person, Book } from 'react-bootstrap-icons';
+import { useTranslation } from 'react-i18next';
+import '../i18n';
 
 function BottomNavbar() {
+    const { t } = useTranslation();
+
+    
+
     return (
         <>
             <Container fluid className="bottom-navigation-bar">
@@ -15,13 +21,14 @@ function BottomNavbar() {
                 <Row className="bottom-section">
                     <Col xs={12} className="text-center main-icon">
                         <Link to="/bookcase">
-                            <Book size={32} />
+                            <Book size={28} />
+                            <p>{t('書櫃')}</p>
                         </Link>
                     </Col>
                     <Col xs={3} className="icon-col">
                         <Link to="/">
                             <HouseDoor size={32} />
-                            <div className="icon-text">Home</div>
+                            <div className="icon-text">{t('首頁')}</div>
                         </Link>
                     </Col>
                     <Col xs={3} className="icon-col">
@@ -30,20 +37,20 @@ function BottomNavbar() {
                             state={{ from: 'homepage' }}
                         >
                             <Grid size={32} />
-                            <div className="icon-text">Category</div>
+                            <div className="icon-text">{t('分類')}</div>
                         </Link>
                     </Col>
                     
                     <Col xs={3} className="icon-col">
                         <Link to="/nftMarket">
                             <Cart size={32} />
-                            <div className="icon-text">Market</div>
+                            <div className="icon-text">{t('NFT市場')}</div>
                         </Link>
                     </Col>
                     <Col xs={3} className="icon-col">
                         <Link to="/readerPage">
                             <Person size={32} />
-                            <div className="icon-text">Profile</div>
+                            <div className="icon-text">{t('個人資訊')}</div>
                         </Link>
                     </Col>
                     
