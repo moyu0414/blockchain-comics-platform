7cc68034296f79840805fb15b85bb71dd02c368f
manzhu <11046073@ntub.edu.tw>
2024-10-28 16:03:27 +0800

    修改我的訊息頁面

--- frontend/src/routes/messagePage.js
+++ frontend/src/routes/messagePage.js
@@ -79,55 +79,55 @@ function MessagePage() {
 
     return (
         <div>
-            {!loading ? (
-                <Container className='messagePage pt-4'>
-                    {!being ? (
-                        <Row className='pt-5 justify-content-center'>
-                            <h1 className="fw-bold text-center">{t('請先收藏漫畫')}</h1>
-                        </Row>
-                    ) : (
-                        <>
-                            {comic.length === 0 ? (
-                                <Row className='pt-5 justify-content-center'>
-                                    <h1 className="fw-bold text-center">{t('目前沒有更新通知')}</h1>
-                                </Row>
-                            ) : (
-                                comic.map((message, index) => (
-                                    <Col key={index} xs={12} lg={6}>
-                                        <Card className="mt-1">
-                                            <div style={{ flex: '1', position: 'relative' }}>
-                                                <Link to={`/comicDetail/${message.comicID}`}>
-                                                    <Card.Img 
-                                                        variant="top" 
-                                                        src={message.image} 
-                                                        alt={message.comicTitle} 
-                                                        style={{ width: '100%', maxWidth: '100%' }} 
-                                                    />
-                                                    {message.level === '限制級' && (
-                                                        <Card.Img src={getImageSrc(language)} className="level" />
-                                                    )}
-                                                </Link>
-                                            </div>
-                                            <div style={{ flex: '2', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                                                <Card.Body className="text-section text-left">
-                                                    <Card.Title>{t('漫畫')}：{message.comicTitle}</Card.Title>
-                                                    <Card.Title>{t('章節更新至')}：{message.chapterTitle}</Card.Title>
-                                                    <Card.Title>{t('更新時間')}：{message.newCreate}</Card.Title>
-                                                </Card.Body>
-                                            </div>
-                                        </Card>
-                                    </Col>
-                                ))
-                            )}
-                        </>
-                    )}
-                </Container>
+    {!loading ? (
+        <Container className='messagePage pt-4'>
+            {!being ? (
+                <Row className='pt-5 justify-content-center'>
+                    <h1 className="fw-bold text-center">{t('請先收藏漫畫')}</h1>
+                </Row>
             ) : (
-                <div className="loading-container">
-                    <div>{t('頁面加載中')}</div>
-                </div>
+                comic.length === 0 ? (
+                    <Row className='pt-5 justify-content-center'>
+                        <h1 className="fw-bold text-center">{t('目前沒有更新通知')}</h1>
+                    </Row>
+                ) : (
+                    <Row>
+                        {comic.map((message, index) => (
+                            <Col key={index} xs={12} md={6} lg={4}>
+                                <Card className="mt-1">
+                                    <div style={{ flex: '1', position: 'relative' }}>
+                                        <Link to={`/comicDetail/${message.comicID}`}>
+                                            <Card.Img 
+                                                variant="top" 
+                                                src={message.image} 
+                                                alt={message.comicTitle} 
+                                                style={{ width: '100%', maxWidth: '100%' }} 
+                                            />
+                                            {message.level === '限制級' && (
+                                                <Card.Img src={getImageSrc(language)} className="level" />
+                                            )}
+                                        </Link>
+                                    </div>
+                                    <div style={{ flex: '2', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                                        <Card.Body className="text-section text-left">
+                                            <Card.Title>{t('漫畫')}：{message.comicTitle}</Card.Title>
+                                            <Card.Title>{t('章節更新至')}：{message.chapterTitle}</Card.Title>
+                                            <Card.Title>{t('更新時間')}：{message.newCreate}</Card.Title>
+                                        </Card.Body>
+                                    </div>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                )
             )}
+        </Container>
+    ) : (
+        <div className="loading-container">
+            <div>{t('頁面加載中')}</div>
         </div>
+    )}
+</div>
     );
 }
 
